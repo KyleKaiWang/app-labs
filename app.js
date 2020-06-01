f3548dbaea6d7d9c13722e3e506e37657c9f4bef
Kyle Wang <z82101096@Kyles-MacBook-Pro-2.local>
2020-06-02 07:08:37 +0800

    Completed signup and login flow and authentication

--- app.js
+++ app.js
@@ -2,29 +2,65 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 
 const errorController = require('./controllers/error');
 const mongoose = require('mongoose');
 const app = express();
-
+const MongoDBStore = require('connect-mongodb-session')(session);
+const MONGODB_URI = 'mongodb+srv://dbUser:RAWbRMKkfcCAfjrE@cluster0-0ubpi.mongodb.net/app-labs?retryWrites=true&w=majority';
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+});
+const csrf = require('csurf');
+const csrfProtection = csrf();
+const flash = require('connect-flash');
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+const user = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    session({
+      secret: 'my secret',
+      resave: false,
+      saveUninitialized: false,
+      store: store
+    })
+  );
+  app.use(csrfProtection);
+  app.use(flash());
 app.use((req, res, next) => {
+    if(!req.session.user) {
+        return next();
+    }
+    user.findById(req.session.user._id)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+});
+
+app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
     next();
 });
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 app.use(errorController.get404);
 
-mongoose.connect('mongodb+srv://dbUser:tm4e0PJ46VmOqPBZ@cluster0-0ubpi.mongodb.net/app-labs?retryWrites=true&w=majority')
+mongoose.connect(MONGODB_URI)
     .then(result => {
-        app.listen(3000);
+      app.listen(3000);
     })
     .catch(err => {
         console.log(err);
